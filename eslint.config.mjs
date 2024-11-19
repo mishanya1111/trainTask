043f79f21b5ba0f2228d4b2f6619463af2484682
mishanya1111 <mihail0grebenkin@gmail.com>
2024-11-19 22:51:13 +0300

    fix: fix problem wirh eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,46 +3,60 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import simpleImportSort from "eslint-plugin-simple-import-sort";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-    { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    eslintConfigPrettier,
-
     {
-        'settings': {
-            'react': {
-                'version': 'detect'
-            }
-        }
-    }, {
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'warn',
-            'react/react-in-jsx-scope': 'off',
-            "react/prop-types": 'off',
-
-        }
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     },
     {
         languageOptions: {
-            "sourceType": "module",
-            "ecmaVersion": "latest"
+            globals: { ...globals.browser, ...globals.jest },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
         },
+    },
+    pluginJs.configs.recommended,
+    {
         plugins: {
-            "simple-import-sort": simpleImportSort,
+            react: pluginReact,
         },
         rules: {
-            "simple-import-sort/imports": "error",
-            "simple-import-sort/exports": "error",
+            ...pluginReact.configs.recommended.rules,
         },
-    }
+        settings: {
+            react: {
+                version: 'detect', // Автоматическое определение версии React
+            },
+        },
+    },
+    eslintConfigPrettier,  // для отключения конфликтующих правил с Prettier
+    ...tseslint.configs.recommended,
 
+    // Настройки для сортировки импортов
+    {
+        plugins: {
+            'simple-import-sort': simpleImportSort, // Исправили с массива на объект
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    },
 
+    {
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
 
+    {
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+        },
+    },
 ];
-
