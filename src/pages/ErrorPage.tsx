864b0d0428047eeb61dbe96371bd36716007d26b
mishanya1111 <mihail0grebenkin@gmail.com>
2024-11-21 20:20:08 +0300

    fix: fix error Page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,8 +2,6 @@ import { HOME_PAGE_ROUTE } from '@constants/routes';
 import React, { useCallback } from 'react';
 import { Link, useRouteError } from 'react-router-dom';
 
-const Navbar = React.lazy(() => import('@components/navbar/Root'));
-
 interface RouteError {
     status?: number;
     statusText?: string;
@@ -20,20 +18,21 @@ const ErrorPageComponent: React.FC = () => {
 
     return (
         <React.Suspense fallback={<div>Loading...</div>}>
-            <div className="errorContainer">
-                <Navbar />
-                {error?.status === 404 ? (
-                    <div className="not-found-container">
-                        <h1>404 - Not Found</h1>
-                        <p>The page you are looking for does not exist.</p>
-                        <Link to={HOME_PAGE_ROUTE}>Go to Home</Link>
-                    </div>
-                ) : (
-                    <h1>
-                        {error?.status} - {error?.statusText}
-                    </h1>
-                )}
-            </div>
+            <main id="detail">
+                <div className="errorContainer">
+                    {error?.status === 404 ? (
+                        <div className="not-found-container">
+                            <h1>404 - Not Found</h1>
+                            <p>The page you are looking for does not exist.</p>
+                            <Link to={HOME_PAGE_ROUTE}>Go to Home</Link>
+                        </div>
+                    ) : (
+                        <h1>
+                            {error?.status} - {error?.statusText}
+                        </h1>
+                    )}
+                </div>
+            </main>
         </React.Suspense>
     );
 };
